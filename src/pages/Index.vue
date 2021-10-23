fd91f5b69427335656e33a81fb6a7f026c5e8e90
metalicarus <silva.matheusms.mateus83@gmail.com>
2021-10-23 11:08:40 -0300

    large all charts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,4 @@
 <template>
-
     <q-page class="q-ma-lg">
       <q-card class="q-pa-lg ">
         <q-stepper
@@ -55,83 +54,15 @@
               @stepFive:update="comparables = $event"
             />
           </q-step>
-          <q-step :name="6"
-                  title="Resultados"
-                  icon="addchart"
+          <q-step :name="stepSix.name"
+                  :title="stepSix.title"
+                  :icon="stepSix.icon"
                   >
-                <div class="row">
-                  <q-table
-                    title="Propriedades singualres"
-                    :rows="tableRows"
-                    :columns="tableColumns"
-                    flat
-                  />
-                </div>
-            <div class="row" v-if="step === 6">
-              <div :class="(largeChart === '') ?
-                          'col-sm-12 col-md-12 col-lg-4' :
-                          (largeChart === `bar${property}`) ?
-                          'col-sm-12 col-md-12 col-lg-6' :
-                          'col-sm-12 col-md-12 col-lg-3'"
-                   v-for="(property, index) in arrayableProperties"
-                   :key="index"
-              >
-                <q-btn round
-                       flat
-                       icon="open_in_full"
-                       size="sm"
-                       @click="setLargeChart(`bar${property}`)"
-                />
-                <div v-for="(arr, arrIndex) in arrayableContent" :key="arrIndex">
-                  <apexchart type="bar"
-                             v-if="arr.series[0].property === property"
-                             :series="arr.series"
-                             :options="arr['chartOptions']"
-                             height="auto"
-                             width="100%"
-                  />
-                </div>
-              </div>
-
-            </div>
-            <div class="row" v-if="step === 6">
-              <div :class="(largeChart === '') ?
-                          'col-sm-12 col-md-12 col-lg-4' :
-                          (largeChart === `line${property}`) ?
-                          'col-sm-12 col-md-12 col-lg-6' :
-                          'col-sm-12 col-md-12 col-lg-3'"
-                   v-for="(property, index) in arrayableProperties"
-                   :key="index"
-              >
-                <q-btn round
-                       flat
-                       icon="open_in_full"
-                       size="sm"
-                       @click="setLargeChart(`line${property}`)"
-                />
-                <div v-for="(arr, arrIndex) in arrayableContent" :key="arrIndex">
-                  <apexchart type="line"
-                             v-if="arr.series[0].property === property"
-                             :series="arr.series"
-                             :options="arr['chartOptions']"
-                             height="auto"
-                             width="100%"
-                  />
-                </div>
-              </div>
-
-            </div>
-            <div class="row">
-              <div class="col-sm-12 col-md-12 col-lg-3"
-                   v-for="(struture, index) in structures"
-                   :key="index"
-              >
-                <q-tree node-key="label"
-                        :nodes="[struture]"
-                        :key="index"
-                />
-              </div>
-            </div>
+            <step-six-content
+              :main-content="mainContent"
+              :comparables="comparables"
+              :description="stepSix.description"
+              />
           </q-step>
         </q-stepper>
         <q-card-actions>
@@ -163,9 +94,11 @@ import StepTwoContent from 'components/steps/StepTwoContent';
 import StepThreeContent from 'components/steps/StepThreeContent';
 import StepFourContent from 'components/steps/StepFourContent';
 import StepFiveContent from 'components/steps/StepFiveContent';
+import StepSixContent from 'components/steps/StepSixContent';
 
 export default {
   components: {
+    StepSixContent,
     StepFiveContent,
     StepFourContent,
     StepThreeContent,
@@ -175,95 +108,15 @@ export default {
 
   data() {
     return {
-      largeChart: '',
       mainContent: null,
       comparable: null,
       step: ref(1),
       patients: null,
       comparables: [],
       count: null,
-      options: {},
-      series: [44, 55, 41, 17, 15],
-      arrayableLabels: {
-        chart: {
-          height: 350,
-          width: '100%',
-        },
-        plotOptions: {
-          bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'rounded',
-          },
-        },
-        dataLabels: {
-          enabled: false,
-        },
-        xaxis: {
-          categories: [],
-        },
-        title: {
-          text: null,
-          align: 'center',
-          style: {
-            color: '#444',
-          },
-        },
-      },
-
     };
   },
   computed: {
-    tableColumns() {
-      return this.singleProperties.map((x) => ({
-        name: x,
-        label: x,
-        field: x,
-        sortable: true,
-      }));
-    },
-    tableRows() {
-      const contents = this.comparables.slice();
-      contents.push(this.mainContent);
-      return contents;
-    },
-    structures() {
-      const strutures = [];
-
-      const contents = this.comparables.slice();
-      contents.push(this.mainContent);
-
-      contents.forEach((content, i) => {
-        const keys = Object.keys(content);
-        const obj = {
-          label: i,
-          children: [],
-        };
-        keys.forEach((x) => {
-          if (!Array.isArray(content[x])) {
-            obj.children.push({ label: `${x}: ${content[x]}` });
-          }
-        });
-        strutures.push(obj);
-      });
-      return strutures;
-    },
-    arrayableProperties() {
-      const keys = Object.keys(this.mainContent);
-      const arrayable = [];
-      keys.forEach((key) => {
-        if (Array.isArray(this.mainContent[key])) arrayable.push(key);
-      });
-      return arrayable;
-    },
-    singleProperties() {
-      const keys = Object.keys(this.mainContent);
-      const singles = [];
-      keys.forEach((key) => {
-        if (!Array.isArray(this.mainContent[key])) singles.push(key);
-      });
-      return singles;
-    },
     nextButton() {
       if (this.step === 1) return !(this.patients === null);
       if (this.step === 2) return !(this.mainContent === null);
@@ -317,83 +170,15 @@ export default {
         description: 'Insira os arquivos comparáveis (apenas um arquivo por campo)',
       };
     },
-    arrayableContent() {
-      const series = [];
-      this.arrayableProperties.forEach((property) => {
-        const serie = { property, name: 'Principal', data: {} };
-        const result = { chartOptions: null, series: [] };
-
-        serie.data = this.processMainArrayableValue(this.mainContent[property]);
-        const comparables = this.processComparablesArrayblesValues(property, this.comparables);
-
-        const options = JSON.parse(JSON.stringify(this.arrayableLabels));
-        options.chart.type = 'bar';
-        options.xaxis.categories = Object.getOwnPropertyNames(serie.data);
-        options.title.text = property;
-
-        comparables.props.forEach((prop) => {
-          if (!options.xaxis.categories.includes(prop)) options.xaxis.categories.push(prop);
-        });
-        result.chartOptions = options;
-
-        comparables.series.forEach((comparable) => {
-          const normalized = this.dataNormalize(serie.data, comparable.data);
-          serie.data = normalized.a;
-        });
-
-        comparables.series.forEach((comparable) => {
-          const normalized = this.dataNormalize(serie.data, comparable.data);
-          comparable.data = Object.values(normalized.b);
-        });
-
-        serie.data = Object.values(serie.data);
-        result.series.push(serie);
-        result.series = [...result.series, ...comparables.series];
-
-        series.push(result);
-      });
-      return series;
-    },
-  },
-  methods: {
-    setLargeChart(chart) {
-      this.largeChart = (this.largeChart === chart) ? '' : chart;
-    },
-    dataNormalize(a, b) {
-      Object.getOwnPropertyNames(a).sort().forEach((property) => {
-        if (!(property in b)) b[property] = 0;
-      });
-
-      Object.getOwnPropertyNames(b).sort().forEach((property) => {
-        if (!(property in a)) a[property] = 0;
-      });
+    stepSix() {
       return {
-        a, b,
+        name: 6,
+        title: 'Resultados',
+        icon: 'addchart',
+        description: 'Resultados obtidos',
       };
     },
-    processComparablesArrayblesValues(property, values) {
-      const series = [];
-      let props = [];
-      values.forEach((x, i) => {
-        const serie = { property, name: `Comparável ${i + 1}`, data: {} };
-        serie.data = this.processMainArrayableValue(x[property]);
-        props = [...props, ...Object.getOwnPropertyNames(serie.data)];
-        series.push(serie);
-      });
-      props.filter((item, index) => props.indexOf(item) === index);
-      return {
-        series,
-        props,
-      };
-    },
-    processMainArrayableValue(values) {
-      const data = {};
-      values.forEach((x) => {
-        data[x] = (data[x] || 0) + 1;
-      });
-      return data;
-    },
-
   },
+
 };
 </script>
